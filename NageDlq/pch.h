9233bdbdec130e39f962a9af7271712cf0069f4b
lichenaiai <lichenaiai@163.com>
2026-04-17 21:41:23 +0800

    0.78  版本号改为0.73

--- NageDlq/pch.h
+++ NageDlq/pch.h
@@ -17,7 +17,7 @@
 
 #endif //PCH_H
 
-#define CLIENT_VERSION "0.72"				   // 客户端版本号 小于数据库版本号时执行更新
+#define CLIENT_VERSION "0.73"				   // 客户端版本号 小于数据库版本号时执行更新
 #define UPDATE_SERVER "115.190.243.231/nageup/"  // 更新服务器地址
 #define SERVER_IP "124.220.82.87"              // 登录器服务端IP地址
 #define SERVER_PORT 9896                       // 登录器服务端端口号
